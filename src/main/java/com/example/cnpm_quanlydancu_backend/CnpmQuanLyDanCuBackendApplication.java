8c7d9ff806919db1645dc2ea49e2a3ba3048d8f1
Vu0811 <thevuu2001@gmail.com>
2023-02-03 13:04:09 +0700

    code ubuntu

---
+++ src/main/java/com/example/cnpm_quanlydancu_backend/CnpmQuanLyDanCuBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.cnpm_quanlydancu_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CnpmQuanLyDanCuBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CnpmQuanLyDanCuBackendApplication.class, args);
+    }
+
+}
